39061e29f72b82c332d221f3ddb0832228e0e062
ashutosh <jhaashu09868@gmail.com>
2024-04-18 01:36:03 +0530

    file structure

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // create another method\\
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw err;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw err;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (e) {
+      console.log("Appwrite :: service :: getcurrentUser", e);
+    }
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (e) {
+      console.log("Appwrite :: Service :: logout ", e);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
