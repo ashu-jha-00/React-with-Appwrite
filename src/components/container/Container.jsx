c68b548ebd0a2c8e61bf030ec04fef404eb3f981
ashutosh <jhaashu09868@gmail.com>
2024-04-22 01:41:20 +0530

    input button created

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const container = ({ children }) => {
+    return (
+        <div className='w-full max-w-7xl mx-auto px-4'>container</div>
+    )
+}
+
+export default container
